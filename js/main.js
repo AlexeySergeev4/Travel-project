2d2fd2a7b3d55fa67f26fe1455d55ff16231ebbf
AlexeySergeev4 <115408253+AlexeySergeev4@users.noreply.github.com>
2022-11-30 12:43:00 +0700

    First commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const owl = $(".owl-carousel");
+owl.owlCarousel({
+  center: true,
+  loop: true,
+  margin: 20,
+  startPosition: 1,
+  items: 1,
+  responsive: {
+    850: {
+      items: 3,
+    },
+
+    1200: {
+      margin: 30,
+      items: 3,
+    },
+  },
+});
+
+$(".slider__btn--prev").click(function () {
+  owl.trigger("prev.owl.carousel");
+});
+
+$(".slider__btn--next").click(function () {
+  owl.trigger("next.owl.carousel");
+});
+
+// NAv icon
+
+const navBtn = document.querySelector(".nav__toggle");
+const nav = document.querySelector(".nav");
+const menuIcon = document.querySelector(".menu-icon");
+
+navBtn.onclick = function () {
+  nav.classList.toggle("nav--mobile");
+  menuIcon.classList.toggle("menu-icon-active");
+  document.body.classList.toggle("no-scroll");
+};
